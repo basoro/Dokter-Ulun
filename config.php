13a80a1123c44bf821af4b2991ee24c78f5f081f
Basoro <dentix.id@gmail.com>
2018-12-10 14:50:17 +0800

    Update config.php

--- config.php
+++ config.php
@@ -26,8 +26,9 @@ define('DIR', '');
 define('PRODUCTION', 'YES'); // YES to hide error page. NO to display error page.
 define('INACBG_KEYRS', '');
 define('INACBG_URLWS', '');
+define('NO_RM', '999999');
 define('NO_PESERTA', '99999999');
-define('SEP', '99999999');
+define('SEP', '9999999999');
 define('NIK_KODER', '99999999999999');
 define('TIPE_RS', 'C');
 
