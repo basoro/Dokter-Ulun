ff09b54d5427916984e9febc40d84c64370b4e15
tiredbug <drg.faisol@basoro.org>
2019-01-20 12:21:30 +0800

    Update

--- layout/sidebar.php
+++ layout/sidebar.php
@@ -20,7 +20,7 @@
                     <div class="btn-group user-helper-dropdown">
                         <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
                         <ul class="dropdown-menu pull-right">
-                            <li><a href="javascript:void(0);"><i class="material-icons">person</i>Profile</a></li>
+                            <li><a href="profile.php"><i class="material-icons">person</i>Profile</a></li>
                             <li role="seperator" class="divider"></li>
                             <li><a href="login.php?action=logout"><i class="material-icons">input</i>Sign Out</a></li>
                         </ul>
